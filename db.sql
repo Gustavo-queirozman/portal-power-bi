2b0cea5c17f604e9912d255ebd1442b82b320d0d
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-05-15 08:08:20 -0300

    update sql

--- db.sql
+++ db.sql
@@ -7,7 +7,7 @@ use db_painel;
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12/05/2023 às 20:49
+-- Tempo de geração: 15/05/2023 às 13:06
 -- Versão do servidor: 10.4.27-MariaDB
 -- Versão do PHP: 8.1.12
 
@@ -21,7 +21,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-
 --
 -- Banco de dados: `db_painel`
 --
@@ -29,9 +28,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `users`
+-- Estrutura para tabela `password_reset_tokens`
 --
 
+CREATE TABLE `password_reset_tokens` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `password_reset_tokens`
+--
+
+INSERT INTO `password_reset_tokens` (`id`, `email`, `token`, `created_at`) VALUES
+(1, 'suporteti3@unimednet.com.br', '$2y$10$opsbS878z4OPY5ACihPfPeoNHa6Hc/ab3y6czVb..56KZ7UGNB/Gq', '2023-05-15 11:04:17');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
@@ -56,6 +74,12 @@ INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `remember_to
 --
 
 --
+-- Índices de tabela `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices de tabela `users`
 --
 ALTER TABLE `users`
@@ -66,6 +90,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT para tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `users`
